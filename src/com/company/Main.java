18f1e721ea58bae805f251d74b00072ddd24f1d8
ottonik <diabel002@gmail.com>
2021-04-07 20:01:15 +0200

    first commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Maciej Wrzałek ");
         System.out.println("Sebastian Żabiński ");
+        System.out.println("Michał Chrzanowski ");
     }
 
 }
